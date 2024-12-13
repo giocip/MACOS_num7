3333df4a3a7a19892b0fcdcee4d16cdd9ce3ada3
num7 <81037891+giocip@users.noreply.github.com>
2024-12-13 12:02:13 +0100

    test_num7_eligibility.cpp
    g++ test_num7_eligibility.cpp num7.cpp -o test_num7_eligibility

---
+++ test_num7_eligibility.cpp
@@ -0,0 +1,24 @@
+  //test_num7_eligibility.cpp
+ //compiling command: g++ test_num7_eligibility.cpp num7.cpp -o test_num7_eligibility
+//it checks computing system reliability
+#include "num7.h"
+#include <time.h>
+using namespace num7;
+
+int main() {
+        clock_t tic = clock(), toc = tic;
+
+        tic = clock();
+
+        test_num7();
+
+        toc = clock();
+
+    printf("\n--------------------\n");
+    printf("ET: %.6fs", (double)(toc - tic) / CLOCKS_PER_SEC);
+    time_t now = time(NULL);
+    char* s_now = ctime(&now);
+    printf("\n%s*** PROGRAM OVER ***\n", s_now);
+
+    return 0;
+}
